476e90c2bd05aea087bdf0568a4aa4fe659ca360
Md Emarat Hossain <emarathossain00@gmail.com>
2025-05-29 18:27:44 +0600

    Added login session storage setup with Playwright

--- playwright.config.js
+++ playwright.config.js
@@ -30,6 +30,9 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    // baseURL: 'http://192.168.1.125:30333/', for all project
+    // for all project
+    // storageState: 'storage/loginAuth.json',
     trace: 'on-first-retry',
   },
 
@@ -37,18 +40,21 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: 'storage/loginAuth.json',
+      },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -78,4 +84,3 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
